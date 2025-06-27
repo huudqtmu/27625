55ea9185a8e2eafbca2861e83028ec42713f6cc5
Huu Dang Quoc <huudq.vn@gmail.com>
2025-06-27 07:12:24 +0700

    v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P27625.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
